3f7e9c6a6b82f4515133289088d932eb12adec81
崔洪铭 <hongming.cui@wenge.com>
2024-04-15 10:58:07 +0800

    first commit

---
+++ 2.ts/src/1.basic-type.ts
@@ -0,0 +1,13 @@
+let isDone: boolean = false;
+let age: number = 18;
+let firstName: string = 'Tom';
+let message: string = `hello, ${firstName}!`
+let u: undefined = undefined;
+let n: null = null
+let notSure: any = 4;
+notSure = 'maybe a string instead';
+notSure = false;
+
+let list: number[] = [1,2,3]
+// 元组 将不同的数据类型整合到一起
+let x: [string, number] = ['hello', 10];
